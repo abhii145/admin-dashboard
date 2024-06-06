1595cc8ce5480b08237640c3feff91c8815bf5a9
Abhishek Yadav <55094244+abhii145@users.noreply.github.com>
2024-06-06 11:11:59 +0530

    charts created for admin page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -6,6 +6,23 @@ const Transactions = lazy(() => import("./Transactions"));
 const Customers = lazy(() => import("./Customers"));
 const NewProducts = lazy(() => import("./management/NewProducts"));
 const ProductManagement = lazy(() => import("./management/ProductManagement"));
-const TransactionManagement = lazy(() => import("./management/TransactionManagement"));
+const TransactionManagement = lazy(
+  () => import("./management/TransactionManagement")
+);
 
-export { DashBoard, Products, Transactions, Customers, NewProducts, ProductManagement, TransactionManagement};
+const BarCharts = lazy(() => import("./charts/BarCharts"));
+const LineCharts = lazy(() => import("./charts/LineCharts"));
+const PieCharts = lazy(() => import("./charts/PieCharts"));
+
+export {
+  DashBoard,
+  Products,
+  Transactions,
+  Customers,
+  NewProducts,
+  ProductManagement,
+  TransactionManagement,
+  BarCharts,
+  LineCharts,
+  PieCharts,
+};
